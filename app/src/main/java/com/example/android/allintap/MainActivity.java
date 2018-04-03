4465395a9da5dc4e3ac09229a187021f2c36bee8
NIKUNJKUMAR KHANDAR <khandar108nikunj@gmail.com>
2018-04-03 22:57:41 +0530

    first commit

---
+++ app/src/main/java/com/example/android/allintap/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.allintap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://allintap.blogspot.in");
+    }
+}
